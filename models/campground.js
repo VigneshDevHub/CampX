5caca9988b5f9db8ee6a83a3c9e7438373696e9b
Vignesh025 <vigneshjayakumar9221@gmail.com>
2024-08-07 22:28:11 +0530

    .gitignore is now working

--- models/campground.js
+++ models/campground.js
@@ -35,4 +35,4 @@ CampgroundSchema.post('findOneAndDelete', async function(doc){
     }
 })
 
-module.exports=mongoose.model('Campground',CampgroundSchema);
+module.exports=mongoose.model('Campground',CampgroundSchema); 
