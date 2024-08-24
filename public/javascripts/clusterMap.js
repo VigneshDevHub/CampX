1663162c074d606c309dc765b838959657c0ab31
Vignesh025 <vigneshjayakumar9221@gmail.com>
2024-08-24 13:18:57 +0530

    UI changes to dark

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -2,7 +2,7 @@ mapboxgl.accessToken = mapToken;
     const map = new mapboxgl.Map({
         container: 'cluster-map',
         // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
-        style: 'mapbox://styles/mapbox/light-v11',
+        style: 'mapbox://styles/mapbox/dark-v11',
         center: [78.9629, 20.5937],
         zoom: 3
     });
--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,7 +1,7 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/light-v11', // style URL
+    style: 'mapbox://styles/mapbox/dark-v11', // style URL
     center: campground.geometry.coordinates, // starting position [lng, lat]
     zoom: 10, // starting zoom
 });
