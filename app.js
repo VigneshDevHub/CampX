4fcd925d2fe63d22cdcebf172c2785c4df6a7e91
RanitDERIA <ranitderiaofficial@gmail.com>
2024-10-03 23:30:00 +0530

    Enhance navbar

--- app.js
+++ app.js
@@ -1,46 +1,46 @@
-if(process.env.NODE_ENV !== "production"){
+if (process.env.NODE_ENV !== "production") {
     require('dotenv').config();
 }
 
 const express = require('express');
-const path= require('path');
+const path = require('path');
 const mongoose = require('mongoose');
-const ejsMate=require('ejs-mate');
-const session=require('express-session');
-const flash=require('connect-flash');
-const ExpressError= require('./utils/ExpressError');
-const methodOverride=require('method-override');
-const passport=require('passport');
-const LocalStrategy=require('passport-local');
+const ejsMate = require('ejs-mate');
+const session = require('express-session');
+const flash = require('connect-flash');
+const ExpressError = require('./utils/ExpressError');
+const methodOverride = require('method-override');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
 const User = require('./models/user');
 const helmet = require('helmet');
 const MongoStore = require('connect-mongo');
 const mongoSanitize = require('express-mongo-sanitize');
 
-const userRoutes= require('./routes/users');
-const campgroundRoutes=require('./routes/campgrounds');
-const reviewRoutes=require('./routes/reviews');
-const dbUrl=process.env.DB_URL || 'mongodb://127.0.0.1:27017/campx';
+const userRoutes = require('./routes/users');
+const campgroundRoutes = require('./routes/campgrounds');
+const reviewRoutes = require('./routes/reviews');
+const dbUrl = process.env.DB_URL || 'mongodb://127.0.0.1:27017/campx';
 
 mongoose.connect(dbUrl);
 
-const db=mongoose.connection;
-db.on("error",console.error.bind(console,"connection error:"));
-db.once("open", ()=>{
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
     console.log("Database connected");
 });
 
-const app= express();
+const app = express();
 
-app.engine('ejs',ejsMate);
-app.set('view engine','ejs');
-app.set('views',path.join(__dirname, 'views'))
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
-app.use(express.static(path.join(__dirname,'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(mongoSanitize({
-    replaceWith:'_'
+    replaceWith: '_'
 }));
 
 const store = MongoStore.create({
@@ -51,23 +51,24 @@ const store = MongoStore.create({
     }
 });
 
-store.on("error",function(e){
-    console.log("SESSION STORE ERROR",e);
-})
+store.on("error", function (e) {
+    console.log("SESSION STORE ERROR", e);
+});
 
 const sessionConfig = {
     store,
-    name:'session',
-    secret:'thisshouldbeabettersecret!',
+    name: 'session',
+    secret: 'thisshouldbeabettersecret!',
     resave: false,
     saveUninitialized: true,
-    cookie:{
+    cookie: {
         httpOnly: true,
-        // secure:true,
-        expires:Date.now()+1000*60*60*24*7,
-        maxAge:1000*60*60*24*7
+        // secure: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
     }
-}
+};
+
 app.use(session(sessionConfig));
 app.use(flash());
 app.use(helmet());
@@ -109,7 +110,7 @@ app.use(
                 "'self'",
                 "blob:",
                 "data:",
-                "https://res.cloudinary.com/dgr21eiov/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+                "https://res.cloudinary.com/dgr21eiov/", // SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
                 "https://images.unsplash.com/",
             ],
             fontSrc: ["'self'", ...fontSrcUrls],
@@ -124,36 +125,34 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use((req,res,next)=>{
-    res.locals.currentUser=req.user;
-    res.locals.success=req.flash('success');
-    res.locals.error=req.flash('error');
+app.use((req, res, next) => {
+    res.locals.currentUser = req.user;
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    // Set the current path based on the request URL
+    res.locals.currentPath = req.originalUrl; 
     next();
-})
+});
 
+// Routes
+app.use('/', userRoutes);
+app.use('/campgrounds', campgroundRoutes);
+app.use('/campgrounds/:id/reviews', reviewRoutes);
 
-app.use('/',userRoutes);
-app.use('/campgrounds',campgroundRoutes);
-app.use('/campgrounds/:id/reviews',reviewRoutes);
-
-
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.render('home');
 });
 
-
-
-
-app.all('*',(req,res,next)=>{
-    next(new ExpressError('Page Not Found',404));
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404));
 });
 
-app.use((err,req,res,next)=>{
-    const{statusCode=500}=err;
-    if(!err.message) err.message='Something Went Wrong!';
-    res.status(statusCode).render('error',{err});
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'Something Went Wrong!';
+    res.status(statusCode).render('error', { err });
 });
 
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log('Serving on port 3000');
 });
