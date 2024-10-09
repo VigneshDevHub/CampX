61965dad711ab00981a44ef1ea11028696a0b366
MOHINI1403 <mohinichauhan9998@outlook.com>
2024-10-09 22:08:27 +0530

    Adding the Access and Refresh Token

--- models/user.js
+++ models/user.js
@@ -7,6 +7,10 @@ const UserSchema= new Schema({
         type: String,
         required:true
     },
+    password:{
+        type:String,
+        required:true
+    },
     role: {
         type: String,
         enum: ['user', 'admin'],  // Define enum for the role field
