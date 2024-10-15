21596201a2705071ec22e6764dcff359f152ae15
Gauravtb2253 <gtbomble225@gmail.com>
2024-10-15 15:01:36 +0530

    paging added to campgrounds

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,147 +1,187 @@
-const Campground=require('../models/campground');
+const Campground = require("../models/campground");
 const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
 const mapBoxToken=process.env.MAPBOX_TOKEN;
 const geocoder = mbxGeocoding({accessToken:mapBoxToken});
-const {cloudinary} = require('../cloudinary');
-const { query } = require('express');
-const mongoose = require('mongoose');
-const ExpressError = require('../utils/ExpressError.js');
+const { cloudinary } = require("../cloudinary");
+const { query } = require("express");
+const mongoose = require("mongoose");
+const ExpressError = require("../utils/ExpressError.js");
 
-const turf = require('@turf/turf');
-const Review = require('../models/review');
+const turf = require("@turf/turf");
+const Review = require("../models/review");
 
 module.exports.index = async (req, res) => {
-    const { sort } = req.query;
-    // console.log("Sort Query Parameter: ", sort);  
-    let campgrounds;
-    if (sort === 'priceAsc') {
-    
-        campgrounds = await Campground.find({}).populate('reviews').sort({ price: 1 }).lean();
-    } else if (sort === 'priceDesc') {
-        
-        campgrounds = await Campground.find({}).populate('reviews').sort({ price: -1 }).lean();
+  const { sort, page = 1 } = req.query;
+  const limit = 9;
+  const skip = (page - 1) * limit;
+
+  let campgrounds;
+  if (sort === "priceAsc") {
+    campgrounds = await Campground.find({})
+      .populate("reviews")
+      .sort({ price: 1 })
+      .skip(skip)
+      .limit(limit)
+      .lean();
+  } else if (sort === "priceDesc") {
+    campgrounds = await Campground.find({})
+      .populate("reviews")
+      .sort({ price: -1 })
+      .skip(skip)
+      .limit(limit)
+      .lean();
+  } else {
+    campgrounds = await Campground.find({}).populate("reviews").lean();
+
+    if (sort === "reviewsHighest") {
+      campgrounds = campgrounds
+        .map((campground) => {
+          const ratings = campground.reviews.map((review) => review.rating);
+          const averageRating = ratings.length
+            ? ratings.reduce((sum, rating) => sum + rating, 0) / ratings.length
+            : 0;
+          return { ...campground, averageRating };
+        })
+        .sort((a, b) => b.averageRating - a.averageRating)
+        .slice(skip, skip + limit);
+    } else if (sort === "reviewsLowest") {
+      campgrounds = campgrounds
+        .map((campground) => {
+          const ratings = campground.reviews.map((review) => review.rating);
+          const averageRating = ratings.length
+            ? ratings.reduce((sum, rating) => sum + rating, 0) / ratings.length
+            : 0;
+          return { ...campground, averageRating };
+        })
+        .sort((a, b) => a.averageRating - b.averageRating)
+        .slice(skip, skip + limit);
     } else {
-        
-        campgrounds = await Campground.find({}).populate('reviews').lean();
-
-        if (sort === 'reviewsHighest') {
-          
-            campgrounds = campgrounds.map(campground => {
-                const ratings = campground.reviews.map(review => review.rating);
-                const averageRating = ratings.length
-                    ? ratings.reduce((sum, rating) => sum + rating, 0) / ratings.length
-                    : 0;
-                return { ...campground, averageRating };
-            }).sort((a, b) => b.averageRating - a.averageRating); 
-        } else if (sort === 'reviewsLowest') {
-          
-            campgrounds = campgrounds.map(campground => {
-                const ratings = campground.reviews.map(review => review.rating);
-                const averageRating = ratings.length
-                    ? ratings.reduce((sum, rating) => sum + rating, 0) / ratings.length
-                    : 0;
-                return { ...campground, averageRating };
-            }).sort((a, b) => a.averageRating - b.averageRating); 
-        }
+      campgrounds = campgrounds.slice(skip, skip + limit);
     }
+  }
 
-    res.render('campgrounds/index', { campgrounds, query: req.query });
+  const totalCampgrounds = await Campground.countDocuments();
+  const totalPages = Math.ceil(totalCampgrounds / limit);
+
+  res.render("campgrounds/index", {
+    campgrounds,
+    query: req.query,
+    currentPage: parseInt(page),
+    totalPages,
+  });
 };
+
 module.exports.searchCampgrounds = async (req, res) => {
-    const { q } = req.query;
-    const campgrounds = await Campground.find({ title: new RegExp(q, 'i') });
-    res.render('campgrounds/index', { campgrounds });
+  const { q } = req.query;
+  const campgrounds = await Campground.find({ title: new RegExp(q, "i") });
+  res.render("campgrounds/index", { campgrounds });
 };
-module.exports.renderNewForm = (req,res)=>{
-    res.render('campgrounds/new');
-}
+module.exports.renderNewForm = (req, res) => {
+  res.render("campgrounds/new");
+};
 
 module.exports.createCampground = async (req, res, next) => {
-    try {
-        const geoData=await geocoder.forwardGeocode({
-            query: req.body.campground.location,
-            limit:1
-        }).send()
-        const campground=new Campground(req.body.campground);
-        campground.geometry=geoData.body.features[0].geometry;
-        campground.images = req.files.map(f=>({url:f.path,filename: f.filename}));
-        campground.author =req.user._id;
-        await campground.save();
-        console.log(campground);
-        req.flash('success','Successfully made a new campground!');
-        res.redirect(`/campgrounds/${campground._id}`);
-    }
-    catch (error) {
-        const err=new ExpressError('Bad Gateway',502)
-        res.status(502).render('error',{err})
-    }
-}
+  try {
+    const geoData = await geocoder
+      .forwardGeocode({
+        query: req.body.campground.location,
+        limit: 1,
+      })
+      .send();
 
-module.exports.showCampground = async (req, res) => {
-    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
-        //Invalid object Id
-        const err=new ExpressError('This URL is not supported',400)
-        return res.status(400).render('error',{err})
-    }
-    const campground=await Campground.findById(req.params.id).populate({
-        path:'reviews',
-        populate:{
-            path:'author'
-        }
-    }).populate('author');
-    if(!campground){
-        req.flash('error','Cannot find that campground!');
-        return res.redirect('/campgrounds');
-    }
-    res.render('campgrounds/show',{campground});
-}
+    const campground = new Campground(req.body.campground);
 
-module.exports.showNearestCampgrounds = async (req, res) => {
-    const { lat, lng } = req.query;
-    if (!lat || !lng) {
-        req.flash('error', 'Location not found');
-        return res.redirect('/campgrounds');
-    }
-    const campgrounds = await Campground.find({});
-    const userLocation = [parseFloat(lng), parseFloat(lat)];
-    // Calculate the distance between user's location and each campground
-    const sortedCampgrounds = campgrounds.map(campground => {
-        const campgroundLocation = campground.geometry.coordinates;
-        const distance = turf.distance(userLocation, campgroundLocation, { units: 'kilometers' });
-        return { ...campground.toObject(), distance };
-    }).sort((a, b) => a.distance - b.distance); // Sort by distance
-
-    res.render('campgrounds/index', { campgrounds: sortedCampgrounds });
+    campground.geometry = geoData.body.features[0].geometry;
+    campground.images = req.files.map((f) => ({
+      url: f.path,
+      filename: f.filename,
+    }));
+    campground.author = req.user._id;
+    await campground.save();
+    console.log(campground);
+    req.flash("success", "Successfully made a new campground!");
+    res.redirect(`/campgrounds/${campground._id}`);
+  } catch (error) {
+    const err = new ExpressError("Bad Gateway", 502);
+    res.status(502).render("error", { err });
+  }
 };
 
-module.exports.renderEditForm = async(req,res)=>{
-    const campground=await Campground.findById(req.params.id);
-    if(!campground){
-        req.flash('error','Cannot find that campground!');
-        return res.redirect('/campgrounds');
-    }
-    res.render('campgrounds/edit',{campground});
-}
+module.exports.showCampground = async (req, res) => {
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    //Invalid object Id
+    const err = new ExpressError("This URL is not supported", 400);
+    return res.status(400).render("error", { err });
+  }
+  const campground = await Campground.findById(req.params.id)
+    .populate({
+      path: "reviews",
+      populate: {
+        path: "author",
+      },
+    })
+    .populate("author");
+  if (!campground) {
+    req.flash("error", "Cannot find that campground!");
+    return res.redirect("/campgrounds");
+  }
+  res.render("campgrounds/show", { campground });
+};
 
-module.exports.updateCampground = async(req,res)=>{
-    const{id}=req.params;
-    const campground = await Campground.findByIdAndUpdate(id,{...req.body.campground});
-    const imgs=req.files.map(f=>({url:f.path,filename: f.filename}))
-    campground.images.push(...imgs);
-    await campground.save()
-    if (req.body.deleteImages){
-        for(let filename of req.body.deleteImages){
-            await cloudinary.uploader.destroy(filename);
-        }
-        await campground.updateOne({$pull:{images:{filename:{$in:req.body.deleteImages}}}})
-    }
-    req.flash('success','Successfully updated campground!');
-    res.redirect(`/campgrounds/${campground._id}`);
-}
+module.exports.showNearestCampgrounds = async (req, res) => {
+  const { lat, lng } = req.query;
+  if (!lat || !lng) {
+    req.flash("error", "Location not found");
+    return res.redirect("/campgrounds");
+  }
+  const campgrounds = await Campground.find({});
+  const userLocation = [parseFloat(lng), parseFloat(lat)];
+  // Calculate the distance between user's location and each campground
+  const sortedCampgrounds = campgrounds
+    .map((campground) => {
+      const campgroundLocation = campground.geometry.coordinates;
+      const distance = turf.distance(userLocation, campgroundLocation, {
+        units: "kilometers",
+      });
+      return { ...campground.toObject(), distance };
+    })
+    .sort((a, b) => a.distance - b.distance); // Sort by distance
 
-module.exports.deleteCampground = async(req,res)=>{
-    const{id}=req.params;
-    await Campground.findByIdAndDelete(id);
-    req.flash('success','Successfully Deleted campground!');
-    res.redirect('/campgrounds');
-}
+  res.render("campgrounds/index", { campgrounds: sortedCampgrounds });
+};
+
+module.exports.renderEditForm = async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  if (!campground) {
+    req.flash("error", "Cannot find that campground!");
+    return res.redirect("/campgrounds");
+  }
+  res.render("campgrounds/edit", { campground });
+};
+
+module.exports.updateCampground = async (req, res) => {
+  const { id } = req.params;
+  const campground = await Campground.findByIdAndUpdate(id, {
+    ...req.body.campground,
+  });
+  const imgs = req.files.map((f) => ({ url: f.path, filename: f.filename }));
+  campground.images.push(...imgs);
+  await campground.save();
+  if (req.body.deleteImages) {
+    for (let filename of req.body.deleteImages) {
+      await cloudinary.uploader.destroy(filename);
+    }
+    await campground.updateOne({
+      $pull: { images: { filename: { $in: req.body.deleteImages } } },
+    });
+  }
+  req.flash("success", "Successfully updated campground!");
+  res.redirect(`/campgrounds/${campground._id}`);
+};
+
+module.exports.deleteCampground = async (req, res) => {
+  const { id } = req.params;
+  await Campground.findByIdAndDelete(id);
+  req.flash("success", "Successfully Deleted campground!");
+  res.redirect("/campgrounds");
+};
