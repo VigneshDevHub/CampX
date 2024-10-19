96896ad0eb6ee8a63fd970bea3fbedd21607a949
Neeraj <nbisht6791@gmail.com>
2024-10-19 18:43:08 +0530

    make the search bar responsive

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,7 +1,7 @@
 const Campground=require('../models/campground');
 const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
 const mapBoxToken=process.env.MAPBOX_TOKEN;
-const geocoder = mbxGeocoding({accessToken:mapBoxToken});
+// const geocoder = mbxGeocoding({accessToken:mapBoxToken});
 const {cloudinary} = require('../cloudinary');
 const { query } = require('express');
 const mongoose = require('mongoose');
@@ -56,26 +56,26 @@ module.exports.renderNewForm = (req,res)=>{
     res.render('campgrounds/new');
 }
 
-module.exports.createCampground = async (req, res, next) => {
-    try {
-        const geoData=await geocoder.forwardGeocode({
-            query: req.body.campground.location,
-            limit:1
-        }).send()
-        const campground=new Campground(req.body.campground);
-        campground.geometry=geoData.body.features[0].geometry;
-        campground.images = req.files.map(f=>({url:f.path,filename: f.filename}));
-        campground.author =req.user._id;
-        await campground.save();
-        console.log(campground);
-        req.flash('success','Successfully made a new campground!');
-        res.redirect(`/campgrounds/${campground._id}`);
-    }
-    catch (error) {
-        const err=new ExpressError('Bad Gateway',502)
-        res.status(502).render('error',{err})
-    }
-}
+// module.exports.createCampground = async (req, res, next) => {
+//     try {
+//         const geoData=await geocoder.forwardGeocode({
+//             query: req.body.campground.location,
+//             limit:1
+//         }).send()
+//         const campground=new Campground(req.body.campground);
+//         campground.geometry=geoData.body.features[0].geometry;
+//         campground.images = req.files.map(f=>({url:f.path,filename: f.filename}));
+//         campground.author =req.user._id;
+//         await campground.save();
+//         console.log(campground);
+//         req.flash('success','Successfully made a new campground!');
+//         res.redirect(`/campgrounds/${campground._id}`);
+//     }
+//     catch (error) {
+//         const err=new ExpressError('Bad Gateway',502)
+//         res.status(502).render('error',{err})
+//     }
+// }
 
 module.exports.showCampground = async (req, res) => {
     if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
