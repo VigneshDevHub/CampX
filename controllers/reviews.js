1441d63e5093f520b5b9912fb2848af393a74788
Suvadip-sana <suvadipsana602@gmail.com>
2024-10-21 04:01:22 +0530

    Add backend logic in review model for review spamming

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -3,6 +3,12 @@ const Review=require('../models/review');
 
 module.exports.createReview = async(req,res)=>{
     const campground = await Campground.findById(req.params.id);
+
+    const isReviewExist = campground.reviews.find(review => review.author._id.equals(req.user._id));
+    if(isReviewExist){
+        req.flash("error", "You already add a review for this ground!")
+        return res.redirect(`/campgrounds/${campground._id}`);
+    }
     const review= new Review(req.body.review);
     review.author=req.user._id;
     campground.reviews.push(review);
