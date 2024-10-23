0bd810057bc67488639ca644595d328e7eb1892e
Suvadip-sana <suvadipsana602@gmail.com>
2024-10-24 02:32:44 +0530

    Fixed the 'Not found' error

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -2,9 +2,9 @@ const Campground=require('../models/campground');
 const Review=require('../models/review');
 
 module.exports.createReview = async(req,res)=>{
-    const campground = await Campground.findById(req.params.id);
+    const campground = await Campground.findById(req.params.id).populate('reviews.author');
 
-    const isReviewExist = campground.reviews.find(review => review.author._id.equals(req.user._id));
+    const isReviewExist = campground.reviews.find(review => review.author && review.author._id.equals(req.user._id));
     if(isReviewExist){
         req.flash("error", "You already add a review for this ground!")
         return res.redirect(`/campgrounds/${campground._id}`);
