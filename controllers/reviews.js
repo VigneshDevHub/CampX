925ea6523ad47a79d3340c307425892126291b1e
Suvadip-sana <suvadipsana602@gmail.com>
2024-11-08 02:10:29 +0530

    Add backend logic to prevent large comment

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -28,6 +28,12 @@ module.exports.createReview = async (req, res) => {
             return res.redirect(`/campgrounds/${campground._id}`);
         }
 
+        // Prevent form submission if review body is gretter than 700 charecter
+        if(req.body.review.body.length >= 700){
+            req.flash("error", "Comment must be within 700 character!");
+            return res.redirect(`/campgrounds/${campground._id}`);
+        }
+
         // Create a new review using the data from the request body
         const review = new Review(req.body.review);
         review.author = req.user._id; // Set the review's author to the current user
